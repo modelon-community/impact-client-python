4b4195d6645db9ebfca740e70d144c379efa5d60
Emil Fredriksson <emil.fredriksson@modelon.com>
2020-06-26 15:15:20 +0200

    fix: correct artifactory URL

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-poetry config repositories.modelon https://artifactory.modelon.com/artifactory/api/pypi/pypi-release-local/simple
+poetry config repositories.modelon https://artifactory.modelon.com/artifactory/api/pypi/pypi-release-local
 poetry publish --build --repository modelon --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD}
