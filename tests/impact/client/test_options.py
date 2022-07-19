493325a492ba98bddbc6b751e5f82a1cf0b742b7
abhilash_kumar_nair <abhilash.kumar@modelon.com>
2022-07-19 09:43:16 +0000

    feat: added possibility to set defaults for custom functions and specify variable filter as a list of strings

--- tests/impact/client/test_options.py
+++ tests/impact/client/test_options.py
@@ -29,3 +29,10 @@ def test_append_options(
     assert dict(runtime_opts) == {'log_level': 3, 'b': 'hello'}
     assert dict(simulation_opts) == {'ncp': 2000, 'a': 10}
     assert dict(solver_opts) == {'rtol': 0.0001, 'c': 5.0}
+
+
+def test_simulation_options_with_result_filter(simulation_options):
+
+    simulation_opts = simulation_options.with_result_filter(pattern=['*.phi'])
+
+    assert dict(simulation_opts) == {'filter': "['*.phi']", 'ncp': 2000}
