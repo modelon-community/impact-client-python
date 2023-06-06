41c51d04f26c1bdf42470c9fe9d3948b4034d0ca
abhilash_kumar_nair <abhilash.kumar@modelon.com>
2023-06-06 03:54:11 +0000

    fix: added docstrings for status

--- modelon/impact/client/entities/status.py
+++ modelon/impact/client/entities/status.py
@@ -6,10 +6,19 @@ class CaseStatus(Enum):
     states."""
 
     SUCCESSFUL = "successful"
+    """Status for case that has been executed successfully."""
+
     FAILED = "failed"
+    """Status for case execution that has failed."""
+
     CANCELLED = "cancelled"
+    """Status for experiment execution that has not yet started."""
+
     NOT_STARTED = "not_started"
+    """Status for experiment execution that has not yet started."""
+
     STARTED = 'started'
+    """Status for case that has  started execution."""
 
 
 class ExperimentStatus(Enum):
@@ -17,9 +26,16 @@ class ExperimentStatus(Enum):
     states."""
 
     NOTSTARTED = "not_started"
+    """Status for experiment execution that has not yet started."""
+
     CANCELLED = "cancelled"
+    """Status for a cancelled experiment execution."""
+
     DONE = "done"
+    """Status for a completed experiment execution."""
+
     FAILED = "failed"
+    """Status for a failed experiment execution."""
 
 
 class ModelExecutableStatus(Enum):
@@ -27,6 +43,13 @@ class ModelExecutableStatus(Enum):
     info states."""
 
     NOTSTARTED = "not_started"
+    """Status for compilation that has not yet started."""
+
     CANCELLED = "cancelled"
+    """Status for a cancelled compilation."""
+
     SUCCESSFUL = "successful"
+    """Status for a successful compilation."""
+
     FAILED = "failed"
+    """Status for a failed compilation."""
