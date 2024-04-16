2f59f612a024163e1aa122956e770e63200d6f4b
abhilash_kumar_nair <abhilash.kumar@modelon.com>
2024-04-16 07:11:33 +0000

    chore: added structured library for tests

--- tests/files/paths.py
+++ tests/files/paths.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 # Setup of general paths:
 _SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -9,3 +10,9 @@ _TEST_WORKSPACE_PATH = os.path.join(_SCRIPT_PATH, "test_workspaces")
 # Paths for library files
 SINGLE_FILE_LIBRARY_PATH = os.path.join(_TEST_LIBRARIES_PATH, "Single.mo")
 TEST_WORKSPACE_PATH = os.path.join(_TEST_WORKSPACE_PATH, "Test.zip")
+
+
+def get_archived_modelica_lib_path(base_path: str):
+    return shutil.make_archive(
+        os.path.join(base_path, "Dynamic"), "zip", _TEST_LIBRARIES_PATH, "Dynamic"
+    )
