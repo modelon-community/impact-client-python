33d51c03e2924ae481efb22d5f5d1155bda7ecc3
abhilash_kumar_nair <abhilash.kumar@modelon.com>
2024-05-30 18:41:59 +0530

    feat: support for FileURI data type in custom function

--- modelon/impact/client/entities/custom_artifact.py
+++ modelon/impact/client/entities/custom_artifact.py
@@ -2,6 +2,8 @@ import os
 import tempfile
 from typing import Optional, Text, Union
 
+from modelon.impact.client.configuration import Experimental
+from modelon.impact.client.entities.file_uri import CustomArtifactReference
 from modelon.impact.client.entities.interfaces.custom_artifact import (
     CustomArtifactInterface,
 )
@@ -88,3 +90,17 @@ class CustomArtifact(CustomArtifactInterface):
         )
 
         return result_stream
+
+    @Experimental
+    def get_uri(self) -> CustomArtifactReference:
+        """Returns a CustomArtifactReference class.
+
+        Returns:
+            The CustomArtifactReference class object.
+
+        Example::
+
+            artifact_file_uri = artifact.get_uri()
+
+        """
+        return CustomArtifactReference(self._exp_id, self._case_id, self._artifact_id)
